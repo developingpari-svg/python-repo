39df64c1e9c944be506ef50ca3195e4d90e1fb48
developingpari-svg <developingpari@gmail.com>
2025-12-01 20:52:48 +0545

    adding first program

--- hashpassword.py
+++ hashpassword.py
@@ -1,11 +1,21 @@
-password = input("enter the password").strip()
-realpass="".join(password.split())
+choice=input("do you want to decode or encode pass")
+if 'encode' in choice:
+    password = input("enter the password with no space").strip()
 
 
-print(len(realpass))
-if len(realpass)>3:
-    hashpass=realpass[1:]+realpass[0]
-    print(hashpass)
+
+
+    print(len(password))
+    if len(password)>3:
+        hashpass=password[1:]+password[0]
+        print(hashpass)
     # now i am suppsoe to reverse it to make it unpredictable
-    finalhash=hashpass[::-1]
-    print(f'this is the real password {finalhash}')
+        finalhash=hashpass[::-1]
+        print(f'this is the real password {finalhash}')
+else:
+    print("decoding is active")
+    hashpass=input("enter here hash password").strip()
+    finalhashpass=hashpass[1:]+hashpass[0]
+    reversehash=finalhashpass[::-1]
+    
+    print(f'this is your real plane password {reversehash}')
---
+++ practise.py
@@ -0,0 +1,5 @@
+name=input('enter here the name ').strip()
+reversename=name[::-1]
+reversereversename=reversename[::-1]
+print('this is the reverse name ', reversename)
+print(f' this is the reverse reverse name', reversereversename)
